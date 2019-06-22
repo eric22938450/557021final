8ba17023ae3cca36ae0d1896ca6ec47f48fff295
andy <andy870622@gmail.com>
2019-06-22 18:28:06 +0800

    Initial Commit

---
+++ 557021/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  557021
+//
+//  Created by User15 on 2019/6/22.
+//  Copyright © 2019 557021. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
